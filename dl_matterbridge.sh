3fa8f4838c15c5082ba37bd6bdc0dae1f3b8ce12
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2023-03-14 18:00:32 +0100

    Update Matterbridge core
    This PR fixes a recurring problem: https://github.com/42wim/matterbridge/pull/2014

--- dl_matterbridge.sh
+++ dl_matterbridge.sh
@@ -1,3 +1,6 @@
-wget https://github.com/42wim/matterbridge/releases/download/v1.25.2/matterbridge-1.25.2-linux-64bit -O matterbridge
+wget --header "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/42wim/matterbridge/actions/artifacts/594328523/zip -O matterbridge.zip
+unzip matterbridge.zip
+mv matterbridge-v1.26.0-22-gc2b8e298-linux-amd64 matterbridge
+
 chmod +x matterbridge
 supervisord -c $APP_HOME/supervisord.conf
