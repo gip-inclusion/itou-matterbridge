925c1ad46c8f71bb87e42033424f61bbd63f4730
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2023-06-30 16:03:00 +0200

    Upgrade Matterbridge version

--- dl_matterbridge.sh
+++ dl_matterbridge.sh
@@ -1,6 +1,6 @@
-wget --header "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/42wim/matterbridge/actions/artifacts/594328523/zip -O matterbridge.zip
+wget --header "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/42wim/matterbridge/actions/artifacts/634227033/zip -O matterbridge.zip
 unzip matterbridge.zip
-mv matterbridge-v1.26.0-22-gc2b8e298-linux-amd64 matterbridge
+mv matterbridge-v1.26.0-33-g89b0d362-linux-amd64 matterbridge
 
 chmod +x matterbridge
 supervisord -c $APP_HOME/supervisord.conf
