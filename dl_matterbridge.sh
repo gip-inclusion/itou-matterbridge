44a904ec3ab1535e8d7da6ffd9846c1e0c0e5ea2
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2023-07-07 14:53:44 +0200

    Commit Matterbridge binary as Arctifacts are available only 90 days on GitHub

--- dl_matterbridge.sh
+++ dl_matterbridge.sh
@@ -1,4 +1,3 @@
-wget --header "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/42wim/matterbridge/actions/artifacts/634227033/zip -O matterbridge.zip
 unzip matterbridge.zip
 mv matterbridge-v1.26.0-33-g89b0d362-linux-amd64 matterbridge
 
