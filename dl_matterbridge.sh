4535908117e9b9a2803193716189ea7d7f05b2df
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2024-07-05 11:28:02 +0200

    Update Mattermost version.
    https://github.com/42wim/matterbridge/actions/runs/9229951266
    https://github.com/42wim/matterbridge/commit/d16645c9527a649cc92f3defb865cea9400fa307

--- dl_matterbridge.sh
+++ dl_matterbridge.sh
@@ -1,5 +1,5 @@
 unzip matterbridge.zip
-mv matterbridge-v1.26.0-33-g89b0d362-linux-amd64 matterbridge
+mv matterbridge-v1.26.0-51-gd16645c9-linux-amd64 matterbridge
 
 chmod +x matterbridge
 supervisord -c $APP_HOME/supervisord.conf
